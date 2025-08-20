545eb23aee5bf0611c8c753a38fe49b47fbc4a3b
RianneMKoelewijn <76046666+RianneMKoelewijn@users.noreply.github.com>
2025-08-20 13:22:30 +0200

    Update thyroid_app.py

--- thyroid_app.py
+++ thyroid_app.py
@@ -38,8 +38,8 @@ def load_models():
         if col in models["2d"]["data"].columns:
             models["2d"]["data"][col] = pd.to_numeric(models["2d"]["data"][col], errors="coerce")
 
-    st.dataframe(models["3d"]["data"])
-    st.dataframe(models["2d"]["data"])
+    models["3d"]["data"] = st.dataframe(models["3d"]["data"])
+    models["2d"]["data"] = st.dataframe(models["2d"]["data"])
     return models
 
 models = load_models()
@@ -211,6 +211,7 @@ else:
     with col2:
         if submitted:
             show_shap(shap_values, model_key)    
+
 
 
 
